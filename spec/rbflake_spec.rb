45633fefe902992b3dd7022f5ef8ed2599ad7089
hideaki_miyake <presents.111@gmail.com>
2016-01-08 06:44:43 +0000

    add spec

--- spec/rbflake_spec.rb
+++ spec/rbflake_spec.rb
@@ -1,11 +1,17 @@
 require 'spec_helper'
 
 describe Rbflake do
+
   it 'has a version number' do
     expect(Rbflake::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'is generated as Bignum' do
+      expect(Rbflake.new.generate.class).to eq(Bignum)
   end
+
+  it 'is 32 Byte' do
+      expect(Rbflake.new.generate.to_s.bytesize).to eq(32)
+  end
+
 end
